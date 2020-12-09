894e1d7cc3256e5fce10cf60f3b8f10d8b0de80d
Deadshot <rkishore0102@gmail.com>
2020-12-09 19:38:27 +0530

    hello world

---
+++ src/test/java/edu/daemondev/service/PsquareSpringbootServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.daemondev.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsquareSpringbootServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
